25ab56fcd1709895de21d7ba7e74ce1a742dcc35
MakerSquare Student <students@themakersquare.com>
2013-09-13 14:49:14 -0500

    Add deuce, advantage, and win to score method.

--- tennis.rb
+++ tennis.rb
@@ -11,11 +11,9 @@ module Tennis
     end
 
     # Increments the score of the winning player by 1. 
-    def wins_ball(x) 
-        player1.points += 1 if x == 1
-      
-        player2.points +=1 if x == 2
-      
+    def wins_ball(player) 
+      player1.points += 1 if player == 1
+      player2.points += 1 if player == 2  
     end
   
   end
@@ -35,11 +33,22 @@ module Tennis
     end
 
     # Returns the String score for the player. **add 2,3,4
+    # 
+    # Returns deuce if both player and opponent have 3 points (we have access to opponent from attr_accessor). 
+    # 
+    # Returns advantage if both players have at least 3 points, but player has
     def score
       return 'love' if @points == 0
       return 'fifteen' if @points == 1
       return 'thirty' if @points == 2
-      return 'forty' if @points == 3
+      
+      return 'deuce' if @points == 3 && @opponent.points==3
+    
+      return 'advantage' if (@points >= 3 && @opponent.points >= 3) && @points == (@opponent.points + 1)
+      return 'win' if @points >= 3 && @points == (@opponent.points + 2)
+      
+      return 'forty' if @points == 3 
+      
     end
   end
 end
