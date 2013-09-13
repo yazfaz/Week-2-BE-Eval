44b273a5ca0182a5ae41c01314207cc79484b851
MakerSquare Student <students@themakersquare.com>
2013-09-13 13:29:08 -0500

    Test existing code and add to methods so tests pass.

--- tennis.rb
+++ tennis.rb
@@ -9,6 +9,15 @@ module Tennis
       @player1.opponent = @player2
       @player2.opponent = @player1
     end
+
+    # Increments the score of the winning player by 1. 
+    def wins_ball(x) 
+        player1.points += 1 if x == 1
+      
+        player2.points +=1 if x == 2
+      
+    end
+  
   end
 
   class Player
@@ -25,9 +34,12 @@ module Tennis
       @points += 1
     end
 
-    # Returns the String score for the player.
+    # Returns the String score for the player. **add 2,3,4
     def score
       return 'love' if @points == 0
+      return 'fifteen' if @points == 1
+      return 'thirty' if @points == 2
+      return 'forty' if @points == 3
     end
   end
 end
